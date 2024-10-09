1f264d9eff7a6b7874c711ef096368cbc33ae750
fajri1997 <f.a.f.s.m.alhusaini@edu.salford.ac.uk>
2024-10-09 06:06:28 +0100

    first commit22

---
+++ functions2.js
@@ -0,0 +1,54 @@
+/**
+ * isOdd(n):
+ * - receives a number n
+ * - returns true if it's odd, false otherwise
+ *
+ * e.g.
+ * isOdd(7) -> true
+ * isOdd(10) -> false
+ */
+function isOdd(n) {
+  // Your code here
+  return n % 2 !== 0;
+}
+console.log(isOdd(7));
+console.log(isOdd(10));
+
+/**
+ * oddsSmallerThan(n):
+ * - receives a number n
+ * - returns the number of ODD numbers smaller than n
+ *
+ * e.g.
+ * oddsSmallerThan(7) -> 3
+ * oddsSmallerThan(15) -> 7
+ */
+function oddsSmallerThan(n) {
+  // Your code here
+  return Math.floor(n / 2);
+}
+console.log(oddsSmallerThan(7));
+console.log(oddsSmallerThan(15));
+
+/**
+ * squareOrDouble(n):
+ * - receives a number n
+ * - returns its square if it's odd
+ * - returns its double if it's even
+ *
+ * e.g.
+ * squareOrDouble(16) -> 32
+ * squareOrDouble(9) -> 81
+ */
+function squareOrDouble(n) {
+  // Your code here
+  if (n % 2 === 0) {
+    return n * 2;
+  } else if (n % 2 !== 0) {
+    return n * n;
+  } else {
+    return n;
+  }
+}
+console.log(squareOrDouble(16));
+console.log(squareOrDouble(9));
